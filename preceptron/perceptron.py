6ebd8fbbb907c0d5f921bae152319cbbd3f73a09
Liqingfeng <562916584@qq.com>
2019-01-18 14:38:22 +0800

    异或门 多层感知机

--- preceptron/perceptron.py
+++ preceptron/perceptron.py
@@ -2,7 +2,7 @@
 import numpy as np
 
 # 与感知机
-def And(x1, x2):
+def AND(x1, x2):
     A = np.array([x1, x2])
     # 这个是w1 w2 权重，用来衡量输入信号的重要程度
     B = np.array([0.5, 0.5])
@@ -15,7 +15,7 @@ def And(x1, x2):
         return 1
 
 # 或感知机
-def Or(x1, x2):
+def OR(x1, x2):
     A = np.array([x1, x2])
     B = np.array([0.5, 0.5])
     b = -0.4
@@ -26,7 +26,7 @@ def Or(x1, x2):
         return 1
 
 # 与非感知机
-def NAnd(x1, x2):
+def NAND(x1, x2):
     A = np.array([x1, x2])
     B = np.array([-0.5, -0.5])
     b = 1.0
@@ -35,6 +35,14 @@ def NAnd(x1, x2):
         return 0
     else:
         return 1
+
+# 单层的感知机无法分离出非线性空间，例如异或门
+# 采用多层感知机表示
+def XOR(x1, x2):
+    A = NAND(x1, x2)
+    B = OR(x1, x2)
+    res = AND(A, B)
+    return res
 
 if __name__=="__main__":
     pass
